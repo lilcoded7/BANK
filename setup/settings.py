6c7ec1253a11527ccd00303cd61b859a5db738c8
coded <coded@gmail.com>
2025-09-18 10:53:42 +0000

    updated

--- setup/settings.py
+++ setup/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6c1=bg3=+k+q6+#h#%lr%x4e)5n!!jz_if&h&7bw2b91xh$-i@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
